b8dc00cb75282f86f40e3fcacad3447fccc0cd17
Ilan Joselevich <personal@ilanjoselevich.com>
2023-03-23 20:21:13 +0200

    ci: dump jsons in "schemas" branch

--- flake.nix
+++ flake.nix
@@ -6,9 +6,17 @@
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, config, ... }: {
       systems = [ "x86_64-linux" ];
       imports = [ inputs.hercules-ci-effects.flakeModule ];
+
+      perSystem = { pkgs, config, ... }: {
+        legacyPackages.generateSchema = pkgs.callPackage ./generator.nix { };
+
+        packages = builtins.mapAttrs (provider: _: config.legacyPackages.generateSchema [ provider ]) pkgs.terraform-providers.actualProviders // {
+          all-schemas = config.legacyPackages.generateSchema (builtins.attrNames pkgs.terraform-providers.actualProviders);
+        };
+      };
 
       hercules-ci.flake-update = {
         enable = true;
@@ -20,12 +28,18 @@
         };
       };
 
-      perSystem = { pkgs, config, ... }: {
-        legacyPackages.generateSchema = pkgs.callPackage ./generator.nix { };
-
-        packages = builtins.mapAttrs (provider: _: config.legacyPackages.generateSchema [ provider ]) pkgs.terraform-providers.actualProviders // {
-          all-schemas = config.legacyPackages.generateSchema (builtins.attrNames pkgs.terraform-providers.actualProviders);
-        };
+      herculesCI = herculesCI: {
+        onPush.default.outputs.effects.dump-to-branch = withSystem config.defaultEffectSystem ({ pkgs, config, hci-effects, ... }:
+          hci-effects.runIf (herculesCI.config.repo.branch == "master") (hci-effects.gitWriteBranch {
+            git.checkout.remote.url = herculesCI.config.repo.remoteHttpUrl;
+            git.checkout.forgeType = "github";
+            git.checkout.user = "x-access-token";
+            git.update.branch = "schemas";
+            contents = pkgs.runCommand "all-schemas-no-link" { } ''
+              mkdir "$out" 
+              cp ${config.packages.all-schemas}/* "$out";
+            '';
+          }));
       };
-    };
+    });
 }
