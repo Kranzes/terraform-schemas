e3a7767246561be27f8f8222ecffc106924dd593
Ilan Joselevich <personal@ilanjoselevich.com>
2023-03-23 15:36:22 +0200

    ci: init

--- flake.nix
+++ flake.nix
@@ -2,19 +2,30 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixpkgs"; };
+    hercules-ci-effects = { url = "github:hercules-ci/hercules-ci-effects"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
+      imports = [ inputs.hercules-ci-effects.flakeModule ];
 
-      perSystem = { pkgs, config, ... }:
-        {
-          legacyPackages.generateSchema = pkgs.callPackage ./generator.nix { };
-
-          packages = builtins.mapAttrs (provider: _: config.legacyPackages.generateSchema [ provider ]) pkgs.terraform-providers.actualProviders // {
-            all-schemas = config.legacyPackages.generateSchema (builtins.attrNames pkgs.terraform-providers.actualProviders);
-          };
+      hercules-ci.flake-update = {
+        enable = true;
+        autoMergeMethod = "rebase";
+        # Update  everynight at midnight
+        when = {
+          hour = [ 0 ];
+          minute = 0;
         };
+      };
+
+      perSystem = { pkgs, config, ... }: {
+        legacyPackages.generateSchema = pkgs.callPackage ./generator.nix { };
+
+        packages = builtins.mapAttrs (provider: _: config.legacyPackages.generateSchema [ provider ]) pkgs.terraform-providers.actualProviders // {
+          all-schemas = config.legacyPackages.generateSchema (builtins.attrNames pkgs.terraform-providers.actualProviders);
+        };
+      };
     };
 }
